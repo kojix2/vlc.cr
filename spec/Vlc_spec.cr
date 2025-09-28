039c7156ad473d8c1aa358dbe3e144745f27a40c
kojix2 <2xijok@gmail.com>
2025-09-28 14:59:19 +0900

    Add minimal test

--- spec/Vlc_spec.cr
+++ spec/Vlc_spec.cr
@@ -1,9 +1,7 @@
 require "./spec_helper"
 
 describe Vlc do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
+  it "has a version" do
+    Vlc::VERSION.should_not be_nil
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
 require "spec"
-require "../src/Vlc"
+require "../src/vlc"
